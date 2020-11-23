774e59cf8f98e7c0c91eda554185534aee61a664
Gonchi <a20135298@pucp.pe>
2020-11-23 14:27:46 -0800

    edit campaign locations fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -45,13 +45,13 @@ class UserController extends Controller
         $record->fill($request->all());
         Log::info($record->account_id);
         if ($record->account_id) {
-            $newFolder = rtrim(realpath(base_path('adsapi_php')) . "\\$record->account_id", '\\/');
+            $newFolder = realpath(base_path('adsapi_php')) . "/$record->account_id";
             if (!file_exists($newFolder)) {
                 mkdir($newFolder, 0777, true);
-                copy(realpath(base_path('adsapi_php.ini')), $newFolder . '\adsapi_php.ini');
-                $content = file_get_contents($newFolder . '\adsapi_php.ini');
+                copy(realpath(base_path('adsapi_php.ini')), $newFolder . '/adsapi_php.ini');
+                $content = file_get_contents($newFolder . '/adsapi_php.ini');
                 $content = str_replace('$clientCustomerId', $record->account_id, $content);
-                file_put_contents($newFolder . '\adsapi_php.ini', $content);
+                file_put_contents($newFolder . '/adsapi_php.ini', $content);
             }
         }
 
--- resources/js/views/requests/edit.vue
+++ resources/js/views/requests/edit.vue
@@ -234,6 +234,7 @@ export default {
         },
         saveCampaign() {
             if (this.request.state == "Aprobado") this.form.active = true;
+            this.form.locations = JSON.stringify(this.form.locations);
             this.$http
                 .post(`/ads-campaign`, this.form)
                 .then(response => {
